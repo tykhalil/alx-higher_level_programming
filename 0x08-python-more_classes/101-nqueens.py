ba863f59fbc1e91f07b7303bda72d93d0b2f33b8
tykhalil <k.tyoubi1528@uca.ac.ma>
2024-01-31 16:38:01 -0800

    liliooo

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,30 +2,49 @@
 """Solves the N-queens puzzle.
 
 Determines all possible solutions to placing N
-non-attacking queens on an NxN chessboard.
+N non-attacking queens on an NxN chessboard.
 
 Example:
     $ ./101-nqueens.py N
 
 N must be an integer greater than or equal to 4.
-"""
 
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
+
 
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = [[' '] * n for _ in range(n)]
-    return board
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
 
 def board_deepcopy(board):
     """Return a deepcopy of a chessboard."""
     if isinstance(board, list):
-        return [board_deepcopy(row) for row in board]
-    return board
+        return list(map(board_deepcopy, board))
+    return (board)
+
 
 def get_solution(board):
     """Return the list of lists representation of a solved chessboard."""
-    return [[r, c] for r, row in enumerate(board) for c, val in enumerate(row) if val == "Q"]
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
+
 
 def xout(board, row, col):
     """X out spots on a chessboard.
@@ -62,7 +81,7 @@ def xout(board, row, col):
     for r in range(row - 1, -1, -1):
         if c < 0:
             break
-        board[r][c] = "x"
+        board[r][c]
         c -= 1
     # X out all spots diagonally up to the right
     c = col + 1
@@ -79,6 +98,7 @@ def xout(board, row, col):
         board[r][c] = "x"
         c -= 1
 
+
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
 
@@ -92,7 +112,7 @@ def recursive_solve(board, row, queens, solutions):
     """
     if queens == len(board):
         solutions.append(get_solution(board))
-        return solutions
+        return (solutions)
 
     for c in range(len(board)):
         if board[row][c] == " ":
@@ -102,13 +122,14 @@ def recursive_solve(board, row, queens, solutions):
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
 
-    return solutions
+    return (solutions)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
     if int(sys.argv[1]) < 4:
